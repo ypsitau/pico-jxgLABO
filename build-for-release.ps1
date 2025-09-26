b371664cb0fd0e3d8e86fec478b40fd879237025
ypsitau <ypsitau@nifty.com>
2025-09-26 17:18:46 +0900

    updated build script

--- build-for-release.ps1
+++ build-for-release.ps1
@@ -4,15 +4,31 @@ if (Test-Path build-pico) {
 if (Test-Path build-pico2) {
     del build-pico2 -force -recurse
 }
+if (Test-Path build-pico-w) {
+    del build-pico-w -force -recurse
+}
+if (Test-Path build-pico2-w) {
+    del build-pico2-w -force -recurse
+}
 cmake -S . -B build-pico -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DPICO_BOARD=pico
 cmake -S . -B build-pico2 -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DPICO_BOARD=pico2
+cmake -S . -B build-pico-w -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DPICO_BOARD=pico_w
+cmake -S . -B build-pico2-w -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DPICO_BOARD=pico2_w
 cmake --build build-pico --config Release
 cmake --build build-pico2 --config Release
-echo "------------------------------------------------------"
-picotool info -a build-pico/pico-jxgLABO.elf
+cmake --build build-pico-w --config Release
+cmake --build build-pico2-w --config Release
+#echo "------------------------------------------------------"
+#picotool info -a build-pico/pico-jxgLABO.elf
 copy build-pico/pico-jxgLABO.uf2 release/pico-jxgLABO.uf2
-echo "------------------------------------------------------"
-picotool info -a build-pico2/pico-jxgLABO.elf
+#echo "------------------------------------------------------"
+#picotool info -a build-pico2/pico-jxgLABO.elf
 copy build-pico2/pico-jxgLABO.uf2 release/pico2-jxgLABO.uf2
+#echo "------------------------------------------------------"
+#picotool info -a build-pico-w/pico-jxgLABO.elf
+copy build-pico-w/pico-jxgLABO.uf2 release/pico-w-jxgLABO.uf2
+#echo "------------------------------------------------------"
+#picotool info -a build-pico2-w/pico-jxgLABO.elf
+copy build-pico2-w/pico-jxgLABO.uf2 release/pico2-w-jxgLABO.uf2
 echo ""
-echo "created: release/pico-jxgLABO.uf2 release/pico2-jxgLABO.uf2"
+echo "created: release/pico-jxgLABO.uf2 release/pico2-jxgLABO.uf2 release/pico-w-jxgLABO.uf2 release/pico2-w-jxgLABO.uf2"
