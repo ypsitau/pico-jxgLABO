55f60e9a4da0b29284886c6d785afd18ae7f9e1a
ypsitau <ypsitau@nifty.com>
2026-01-22 15:04:57 +0900

    corrected a script

--- release-to-github.ps1
+++ release-to-github.ps1
@@ -1,7 +1,6 @@
-# 引数チェック
 if ($args.Count -eq 0) {
-    Write-Host "Usage: .\release-github.ps1 TAG-NAME"
-    Write-Host "Example: .\release-github.ps1 v1.0.0"
+    Write-Host "Usage: .\release-to-github.ps1 TAG-NAME"
+    Write-Host "Example: .\release-to-github.ps1 1.0.0"
     exit 1
 }
 $tagName = $args[0]
